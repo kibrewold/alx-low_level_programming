6b8c97ee7577bb6299e963daf79752f6a72c4467
kibrewold <kibrewold7@gmail.com>
2023-09-09 14:30:13 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - print with out printf and put
+ * Return:1
+ */
+int main(void)
+{
+	getchar("and that piece of art is useful" \ - \ Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
