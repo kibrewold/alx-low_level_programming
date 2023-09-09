9965d2c6394d5bcadb39cbb8a150b21e2a021ef3
kibrewold <kibrewold7@gmail.com>
2023-09-09 14:37:05 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - print with out printf and put
- * Return:1
+ * main - Entry point
+ *
+ * Return: Always 1 (Sucess)
  */
 int main(void)
 {
-	getchar("and that piece of art is useful" \ - \ Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
 
