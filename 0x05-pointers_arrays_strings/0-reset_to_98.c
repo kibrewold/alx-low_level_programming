816d4455ab47274e3af7eaf9e8297a3117ea8979
kibrewold <kibrewold7@gmail.com>
2023-09-19 14:08:57 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,30 +1,10 @@
 #include "main.h"
-
 /**
- * _strcat - this fuction will concatenates two strings
- * @dest: the string that will modified.
- * @src: the string that ill be concatenated to @des.
- *
- * Return: a pointer to @dest
+ * reaet_to_98 -a function that takes a pointer to an int as parameter
+ * @n: input
+ * Return: n
  */
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-	int index_01 = 0;
-	int index_02 = 0;
-
-	while (dest[index_01] != '\0')
-	{
-		index_01++;
-	}
-
-	while (src[index_02] != '\0')
-	{
-		dest[index_01 + index_02] = src[index_02];
-		index_02++;
-
-	}
-	dest[index_01 + index_02] = '\0';
-
-	return (dest);
+	*n=98;
 }
-
