e0f1f946d9b1f758ddfcca07ce78ef6210371fce
kibrewold <kibrewold7@gmail.com>
2023-10-07 13:29:00 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts- print a string.
+ * @str: string to be printed
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
