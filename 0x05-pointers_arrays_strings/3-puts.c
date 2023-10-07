c78477dbf50f89c38d06b83a91374035474bfa5a
kibrewold <kibrewold7@gmail.com>
2023-10-07 13:43:10 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,10 @@
  */
 void _puts(char *str)
 {
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (str);
+	while (*str > 0)
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar('\n');
 }
