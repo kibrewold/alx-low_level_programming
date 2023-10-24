87f6377e2485be02065b913e023084726a164deb
kibrewold.com <kibrewold7@gmail.com>
2023-10-25 00:34:19 +0300

    more linkedlist

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,19 +8,7 @@
   * 
   * Return: The number of elements in the listint_t list. 
   */
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
+
  size_t listint_len(const listint_t *h) 
  { 
          size_t nodes = 0; 
