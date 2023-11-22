c5e2601587f340fb5cb57eaf38b48ce8a71b60c0
kibrewold <kibrewold7@gmail.com>
2023-11-22 20:57:07 +0300

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts- print a string.
+ * @str: string to be printed
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str > 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
